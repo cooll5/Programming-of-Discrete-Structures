b340ff8a1b461cc4eaecda8d0562e4e9b2f1d772
cooll5 <52448498+cooll5@users.noreply.github.com>
2022-04-28 02:59:51 +0300

    Add files via upload

---
+++ Lab06/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System;
+
+namespace Lab06
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            Graph g = new Graph();
+            int[,] result = g.ReadFile();
+
+            //таблиці суміжності та інцидентності
+            IncidentMatrix matrix = new IncidentMatrix();
+            SumMatrix sumMatrix = new SumMatrix();
+
+            int[,] IncMatrix = matrix.createMatrix(g, result);
+            int[,] sMatrix = sumMatrix.createMatrix(g, result);
+
+            IncidentMatrix.printMatrix(IncMatrix, g);
+            Console.WriteLine();
+            SumMatrix.printMatrix(sMatrix, g);
+
+            g.WriteIncident(IncMatrix);
+            g.WriteSumizhn(sMatrix);
+
+            //7степінь вершини графу
+            int[,] arcs = GraphStat.FindArc(sumMatrix, sumMatrix.createMatrix(g, result));
+
+            Console.WriteLine();
+            for (int i = 1; i < g.GetN() + 1; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{arcs[i, j]}  ");
+                }
+                Console.WriteLine();
+            }
+
+
+            //7висячі
+            Console.WriteLine();
+            GraphStat.isHanging(sumMatrix, arcs);
+
+            //7ізольовані
+            Console.WriteLine();
+            GraphStat.isIsolated(sumMatrix, arcs);
+
+            //8Обхід вглиб
+            Console.WriteLine();
+            DFS dfs = new DFS();
+            dfs.Search(sumMatrix, sumMatrix.createMatrix(g, result), 8);
+            dfs.PrintTable();
+
+            //9Обхід вшир
+            BFS bfs = new BFS();
+            bfs.Search(sumMatrix, sumMatrix.createMatrix(g, result), 8);
+            bfs.PrintTable();
+        }
+    }
+}
