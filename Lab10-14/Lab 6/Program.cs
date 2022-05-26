27268b091e0441ac8619f77ed89ebd983130c951
cooll5 <52448498+cooll5@users.noreply.github.com>
2022-05-26 17:06:22 +0300

    Lab 14 correct works

---
+++ Lab10-14/Lab 6/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            FileService f = new FileService();
+            int[,] result = f.ReadFile();
+
+            iMatrix matrix = new iMatrix();
+            int[,] IMatrix = matrix.createMatrix(f, result);
+            iMatrix.printMatrix(IMatrix, f);
+            f.WriteFileIncident(IMatrix);
+
+            Console.WriteLine("\n");
+
+            sMatrix sMatrix = new sMatrix();
+            int[,] SumMatrix = sMatrix.CreateMatrix(f, result);
+            sMatrix.printMatrix(SumMatrix, f);
+            f.WriteFileSumij(SumMatrix);
+            Console.WriteLine("\n");
+            int[,] degs = CharchactGraf.FindInnerAndOut(sMatrix);
+
+            for (int i = 1; i < f.GetN() + 1; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(degs[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //7
+            Console.WriteLine();
+            Console.WriteLine("Lab - 7");
+            CharchactGraf.isVisyacha(sMatrix, degs);
+            CharchactGraf.isIsolated(sMatrix, degs);
+
+            // 8
+            Console.WriteLine();
+            Console.WriteLine("Lab - 8");
+            DFS dfs = new DFS();
+            dfs.Search(sMatrix, 4);
+            dfs.PrintResult();
+
+            // 9
+            Console.WriteLine();
+            Console.WriteLine("Lab - 9");
+            BFS bfs = new BFS();
+            bfs.Search(sMatrix, 4);
+            bfs.PrintResult();
+
+
+            /*
+                        // 10
+                        Console.WriteLine();
+                        Console.WriteLine("Lab - 10");
+                        WeightMatrix w = new WeightMatrix();
+                        int[,] lab10graph = f.ReadFileWeight();
+                        int[,] lab10 = w.CreateMatrix(f, lab10graph);
+                        WeightMatrix.printMatrix(lab10, f);*/
+
+
+            /*
+                        // 11
+                        Console.WriteLine();
+                        Console.Write("Lab - 11");
+                        FloydPathMatrix floidlab11 = new FloydPathMatrix(w, lab10);
+
+
+                        // 12
+
+                        Console.WriteLine();
+                        Console.WriteLine("Lab - 12");
+                        DeixtraMatrix floidlab12 = new DeixtraMatrix(w, lab10);
+
+
+
+                        //13
+
+                        Console.WriteLine("Lab - 13");
+                        int[,] WeightGraph = f.ReadFileWeight();
+                        sMatrix sMatrix1 = new sMatrix();
+                        int[,] SumMatrix1 = sMatrix1.CreateMatrix(f, WeightGraph);
+                        sMatrix.printMatrix(SumMatrix1, f);
+                        Console.WriteLine();
+
+                        ColoredGraph graphColor = new ColoredGraph(sMatrix1);*/
+
+            //14
+            Console.WriteLine();
+            Console.WriteLine("Lab 14");
+            lab14 lab14 = new lab14(sMatrix);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab10-14/Lab 6/lab14.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    internal class lab14
+    {
+        private bool isSymerical;
+        private bool isIrReflecsive;
+
+        public lab14(sMatrix matrix)
+        {
+            isSymerical = checkSymetrical(matrix);
+            isIrReflecsive = checkIrReflecsive(matrix);
+
+            Console.WriteLine("Symetrical: " + isSymerical);
+            Console.WriteLine("IrReflecsive: " + isIrReflecsive);
+        }
+
+        private bool checkSymetrical(sMatrix sMatrix)
+        {
+            int n = sMatrix.GetN();
+            int[,] matrix = sMatrix.GetMatrix();
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if ((matrix[i, j] == 1 && matrix[j, i] == 0)
+                         ||
+                        (matrix[i, j] == 0 && matrix[j, i] == 1))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool checkIrReflecsive(sMatrix sMatrix)
+        {
+            int n = sMatrix.GetN();
+            int[,] m = sMatrix.GetMatrix();
+
+            bool bool_res = true;
+
+            for (int i = 1; i <= n; ++i)
+            {
+                bool_res = m[i, i] == 1;
+
+                if (!bool_res)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
